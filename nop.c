56d8c1fb59a4f0bbaca5a1e417dc4c56ba30958a
root <t.ntwana@gmail.com>
2024-10-02 05:31:18 -0700

    monty

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+
+ * nop - Function that does literally nothing
+ * @stack: Function that points to the head node pointer of stack
+ * @nline: the line number
+
+ * Return: Nothing.
+ */
+void nop(stack_t **stack, unsigned int nline)
+{
+        (void)stack;
+        (void)nline;
+}
